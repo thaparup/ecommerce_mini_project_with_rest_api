8a0d588c3892a1b786ab75009f150c89f9492fd7
thaparup <thaparup3965@gmail.com>
2024-09-24 17:39:52 +0545

    -made some changes to the navbar, - created register pages

--- firebase-config.ts
+++ firebase-config.ts
@@ -1,10 +1,11 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { env } from "process";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAMQ5jF9YdyJsmuuXo1vfIHs8BPT4HTKlo",
-  authDomain: "eccom-e89f0.firebaseapp.com",
-  projectId: "eccom-e89f0",
+  apiKey: import.meta.env.APIKEY,
+  authDomain: import.meta.env.AUTHDOMAIN,
+  projectId: import.meta.env.PROJECTID,
   storageBucket: "eccom-e89f0.appspot.com",
   messagingSenderId: "755231176432",
   appId: "1:755231176432:web:dc887f948e46ec6072b435",
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,24 +1,86 @@
 import React, { useState } from 'react'
 import Navbar from '../components/Navbar'
+import { db } from './../../firebase-config'
+import { collection, addDoc } from 'firebase/firestore'
+import { toast } from "sonner"
+import { Toaster } from "../components/ui/sonner"
+
+
+
+
 
 const Register = () => {
 
     const [email, setEmail] = useState<string>('')
     const [password, setPassword] = useState<string>('')
-    return (
-        <section className='h-screen overflow-hidden flex flex-col '>
-            <Navbar />
-            <div className=' my-auto w-fit mx-auto bg-white p-8 rounded-md'>
+    const [emailError, setEmailError] = useState<string>('')
+    const [passwordError, setPasswordError] = useState<string>('')
+    const [emailValidationError, setEmailValidationError] = useState<string>('')
 
-                <label htmlFor="" className='block font-medium'>Email</label>
-                <input className='bg-gray-100 w-96 p-2 rounded-md focus:outline-primaryColor' value={email} onChange={(e) => setEmail(e.target.value)} />
-                <label htmlFor="" className="block font-medium mt-2">Password</label>
-                <input type="password" className='bg-gray-100 w-96 p-2 rounded-md block  focus:outline-primaryColor' value={password} onChange={(e) => setPassword(e.target.value)} />
-                <div className='w-full flex justify-center mt-3'>
-                    <button className='bg-primaryColor py-2 px-8 text-white rounded-md font-medium'>Register</button>
-                </div>
-            </div>
-        </section>
+
+    const emailRegex = /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/
+    const data = collection(db, "users")
+
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        if (!email) {
+            setEmailError('Email is required*')
+        }
+
+        if (!password) {
+            setPasswordError("Password is required*")
+        }
+        if (!emailRegex.test(email)) {
+
+            if (email) {
+
+                setEmailValidationError("The email address is not valid!")
+            }
+        }
+
+        // if (!email || !password || !emailValidationError) {
+
+        //     return;
+        // }
+
+        else {
+
+            try {
+
+
+                await addDoc(data, { email: email, password: password })
+                toast("Users registered successfully")
+
+            } catch (error) {
+                console.error("Error adding document:", error);
+                toast('Failed to register user')
+            }
+        }
+
+    }
+    return (
+        <div>
+            <Toaster />
+            <section className='h-screen overflow-hidden flex flex-col '>
+                <Navbar />
+                <div className=' my-auto w-fit mx-auto bg-white p-8 rounded-md'>
+
+                    <form action="" onSubmit={handleSubmit}>
+                        <label htmlFor="" className='block font-medium'>Email</label>
+                        <input className='bg-gray-100 w-96 p-2 rounded-md block mt-2 focus:outline-primaryColor shadow-md shadow-inputBoxShadowColor                                                                    ' value={email} onChange={(e) => setEmail(e.target.value)} />
+                        {emailValidationError && (<div className='text-red-400 font-medium'>{emailValidationError}</div>)}
+                        {emailError && (<div className='text-red-400 font-medium'>{emailError}</div>)}
+                        <label htmlFor="" className="block font-medium mt-4">Password</label>
+                        <input type="password" className='bg-gray-100 w-96 p-2 rounded-md block mt-2  focus:outline-primaryColor shadow-md shadow-inputBoxShadowColor' value={password} onChange={(e) => setPassword(e.target.value)} />
+                        {passwordError && (<div className='text-red-400 font-medium'>{passwordError}</div>)}
+
+                        <div className='w-full flex justify-center mt-5'>
+                            <button type='submit' className=' bg-gradient-to-r from-primaryColor to-purple-400 py-2 px-8 text-white rounded-md font-medium' >Register</button>
+                        </div>
+                    </form>
+                </div >
+            </section >
+        </div>
     )
 }
 
